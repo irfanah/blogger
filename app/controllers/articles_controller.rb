1239f7b53b472cfa8ae2f2c84d734ffee0f50ac9
irfanah <irfan.ahmad@tinyowl.co.in>
2015-09-13 22:16:14 +0530

    adding controller for article show and new

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,29 @@
 class ArticlesController < ApplicationController
+   
    def index
      @articles = Article.all
    end
+   
+   def show
+     @article = Article.find(params[:id])
+   end
+  
+   def new
+     @article = Article.new
+   end
+     
+  def create
+    @article = Article.new
+    @article.title = params[:article][:title]
+    @article.body  = params[:article][:body]
+    @article.save
+    redirect_to article_path(@article)
+  end
+  
+  def delete
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to article_index(@article)
+  end
+       
 end
